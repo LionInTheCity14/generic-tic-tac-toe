e8e1171da54d35c2fd38a4411ebfa908688df963
Varun Pal Singh <singhvarun928@gmail.com>
2023-09-27 15:12:14 +0530

    10rows

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <h1 className='text-center font-bold text-2xl font-mono m-2'>Tic Tac Toe</h1>
 
       <div className="h-screen flex justify-center items-center">
-        <MakeBoard noOfRows={3} />
+        <MakeBoard noOfRows={10} />
       </div>
     </>
   )
